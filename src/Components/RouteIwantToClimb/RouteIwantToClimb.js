2f038f24a41f00110317745f4dca8bc471aa24cb
Aaron Estes <aestesc@gmail.com>
2018-12-16 04:54:14 -0600

    sizing everything up

--- src/Components/RouteIwantToClimb/RouteIwantToClimb.js
+++ src/Components/RouteIwantToClimb/RouteIwantToClimb.js
@@ -37,9 +37,9 @@ export default class RouteIwantToClimb extends Component {
         `/api/routePic/${this.props.elem.id_of_route}/${this.state.user.id}`,
         { imageUrl }
       )
-      .then(results => {
+      this.componentDidUpdate = (results) => {
         this.setState({ user_climbing_image: results.data.picture_of_route });
-      });
+      }
   };
 
   changeRating = newRating => {
@@ -67,7 +67,7 @@ export default class RouteIwantToClimb extends Component {
 
   render() {
     const uploadOptions = {
-      server: process.env.REACT_APP_SERVER,
+      server: `${process.env.REACT_APP_SERVER}`,
       signingUrlQueryParams: { uploadType: 'avatar' }
     };
     const s3Url = `http://rrg-climbing-pics.s3-website-us-east-1.amazonaws.com/`;
@@ -76,7 +76,7 @@ export default class RouteIwantToClimb extends Component {
       <div
         className={this.state.display ? 'display_route' : 'dont_display_route'}
       >
-       <button
+        <button
           id='deleteButton'
           onClick={() => this.props.handleClickDel(this.props.elem.route_id)}
         >
@@ -86,18 +86,18 @@ export default class RouteIwantToClimb extends Component {
           {this.props.route_name} {} {this.props.route_grade}
         </p>
         <StarRatings
-            className='stars'
-            rating={this.state.rating}
-            starRatedColor='yellow'
-            changeRating={this.changeRating}
-            numberOfStars={5}
-            name='rating'
-            starDimension='1em'
-            starSpacing='1px'
-            starEmptyColor='white'
-            starHoverColor='yellow'
-            isSelectable='true'
-          />
+          className='stars'
+          rating={this.state.rating}
+          starRatedColor='yellow'
+          changeRating={this.changeRating}
+          numberOfStars={5}
+          name='rating'
+          starDimension='1em'
+          starSpacing='1px'
+          starEmptyColor='white'
+          starHoverColor='yellow'
+          isSelectable='true'
+        />
         <img className='journalImg' src={this.props.route_img} alt='routeImg' />
         <div className='could_drop'>
           <DropzoneS3Uploader
@@ -105,44 +105,25 @@ export default class RouteIwantToClimb extends Component {
             s3Url={s3Url}
             maxSize={1024 * 1024 * 50}
             upload={uploadOptions}
-            id='imageUploader'
           >
             <img
               id='user_route_pic'
               src={this.props.picture_of_route}
               alt='text'
             />
-            <p
-              id={
-                this.state.user.length === 0
-                  ? 'textInDropzone'
-                  : 'noTextInDropzone'
-              }
-            >
-              Try dropping some files here, or click to select files to upload.
-            </p>
           </DropzoneS3Uploader>
-          
         </div>
         <textarea
           id='logInput'
           onChange={e => this.handleChange(e.target.value)}
         />
 
-       
-
         <div
           id='logButton'
           onClick={() => this.log_Route(this.props.elem.route_id)}
         >
-          Log
+          ''
         </div>
-        {/* <button
-          id='sendButton'
-          onClick={() => this.props.handleClickSend(this.props.elem.route_id)}
-        >
-          Send
-        </button> */}
       </div>
     );
   }
